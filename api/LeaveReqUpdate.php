baef35036c60f75a1ccfb9b303cbb012bd50a7e5
SaileshRokaya <79441756+SaileshRokaya@users.noreply.github.com>
2022-01-10 16:03:28 +0545

    LeaveReq changes on delete,read and update files

---
+++ api/LeaveReqUpdate.php
@@ -0,0 +1,27 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../database.php';
+include_once '../LeaveReq.php';
+
+$database = new Database();
+$db = $database->getConnection();
+$item = new LeaveReq($db);
+
+$item->id = isset($_POST['id']) ? $_POST['id'] : die();
+$item->name = $_POST['name'];
+$item->roll_no = $_POST['roll_no'];
+$item->level = $_POST['level'];
+$item->leave_date = $_POST['leave_date'];
+$item->status = $_POST['status'];
+$item->req_reason = $_POST['req_reason'];
+$item->acc_rej_reason = $_POST['acc_rej_reason'];
+if ($item->updateLeaveReq()) {
+    echo json_encode("Data updated.");
+} else {
+    echo json_encode("Data could not be updated");
+}
